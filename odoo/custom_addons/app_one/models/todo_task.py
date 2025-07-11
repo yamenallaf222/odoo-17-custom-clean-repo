c61fa614cac68f6d188f4395fc2f8398d02a4a13
starseeker222 <yamiyohan2@gmail.com>
2025-07-11 21:38:22 +0300

    todo_task enhanced with: - added new timesheet lines for each todo task. - added archiving to todo task model - added close server action to the todo task model - ensured that values of those timesheet lines are validated with due validations - gave access to the new timesheet_line model in the csv security file

--- odoo/custom_addons/app_one/models/todo_task.py
+++ odoo/custom_addons/app_one/models/todo_task.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
 
 class TodoTask(models.Model):
     _name = 'todo.task'
@@ -14,8 +16,21 @@ class TodoTask(models.Model):
         ('new','New'),
         ('in_progress','In Progress'),
         ('completed','Completed'),
+        ('closed','Closed'),
     ], default='new', tracking=1)
-    
+    estimated_time = fields.Float(required=1, default=10.0)
+    timesheet_line_ids = fields.One2many('timesheet.line', inverse_name='todo_task_id')
+    active = fields.Boolean(default=1)
+
+    @api.constrains('estimated_time')
+    def _check_estimated_time_is_not_less_than_the_total_time_of_timesheet_lines(self):
+        for rec in self:
+            total_time = sum(line.time for line in self.timesheet_line_ids)
+            if rec.estimated_time < total_time:
+                raise ValidationError("Please make sure the total time of Timesheet lines is not greater than estimated time")
+
+
+
     def action_new(self):
         for rec in self:
             print("inside new action")
@@ -32,6 +47,29 @@ class TodoTask(models.Model):
         for rec in self:
             print("inside completed action")
             rec.status = 'completed'
+
+    def action_closed(self):
+        for rec in self:
+            print("inside closed action")
+            rec.status = 'closed'
+
+class TimesheetLine(models.Model):
+    _name = 'timesheet.line'
+    _description = "Timesheet lines"
+
+    date = fields.Date(required=1)
+    description = fields.Text(required=1)
+    time = fields.Float(required=1)
+
+    todo_task_id = fields.Many2one('todo.task')
+
+    @api.constrains('date')
+    def _check_date_not_greater_than_task_date(self):
+        for rec in self:
+            if rec.date > rec.todo_task_id.due_date:
+                raise ValidationError("Please add valid Timesheet lines' time values: not bigger than task's due date")
+
+
 
 
    
