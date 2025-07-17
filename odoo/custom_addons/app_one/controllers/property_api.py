f2333a3ecd70c46d72f32cb6ad2a742db919178b
starseeker222 <yamiyohan2@gmail.com>
2025-07-17 21:13:50 +0300

    experimenting with json type API endpoints as well as setting validating request data in http endpoint type

--- odoo/custom_addons/app_one/controllers/property_api.py
+++ odoo/custom_addons/app_one/controllers/property_api.py
@@ -4,12 +4,46 @@ from odoo.http import request
 
 class PropertyApi(http.Controller):
 
+    def validate_name_field_existence(self, vals):
+        if not vals.get('name'):
+            return False
+   
+
+         
+
     @http.route("/v1/property", method=["POST"], type="http", auth="none", csrf=False)
     def post_property(self):
         args = request.httprequest.data.decode()
         vals = json.loads(args)
+        if not self.validate_name_field_existence(vals):
+            return request.make_json_response({
+                "message": "Name is required!", 
+            }, status=400) 
+        try:
+            res = request.env['property'].sudo().create(vals)
+            if res:
+                return request.make_json_response({
+                    "message": "Property has been created successfully", 
+                    "id" : res.id,
+                    "name" : res.name,
+                    #201 code mean success for creation operation
+                }, status=201)
+        except Exception as error:
+                return request.make_json_response({
+                    "message": error, 
+                }, status=400)
+
+        
+    @http.route("/v1/property/json", method=["POST"], type="json", auth="none", csrf=False)
+    def post_property_json(self):
+        args = request.httprequest.data.decode()
+        vals = json.loads(args)
         res = request.env['property'].sudo().create(vals)
         if res:
-            return request.make_json_response({
+            return [{
                 "message": "Property has been created successfully"
-            }, status=200)
+            }]
+        
+
+
+
