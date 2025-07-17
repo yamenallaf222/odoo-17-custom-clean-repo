a22596bad575db648ef475192e2f05133fecf090
starseeker222 <yamiyohan2@gmail.com>
2025-07-17 19:40:39 +0300

    testing api endpoints

---
+++ odoo/custom_addons/app_one/controllers/test_api.py
@@ -0,0 +1,9 @@
+from odoo import http
+
+
+class TestApi(http.Controller):
+
+
+    @http.route("/api/test", methods=["GET"], type="http", auth="none", csrf=False)
+    def test_endpoint(self):
+        print("inside test_endpoint method")
