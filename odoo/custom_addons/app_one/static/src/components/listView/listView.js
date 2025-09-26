d79d9572305ad65394cda9bc2db1c9b247dc999a
starseeker222 <yamiyohan2@gmail.com>
2025-09-26 20:46:06 +0300

    kickstarting the usage owl for custom fronted views

--- odoo/custom_addons/app_one/static/src/components/listView/listView.js
+++ odoo/custom_addons/app_one/static/src/components/listView/listView.js
@@ -1,11 +1,27 @@
 /* @odoo-module */
 
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 export class ListViewAction extends Component {
     static template = "app_one.ListView";
+
+    // this is more of a constructor with no args
+    setup() {
+        this.state = useState({
+            'records': []
+        });
+        this.orm = useService("orm");
+        this.loadRecords();
+    };
+
+    async loadRecords() {
+        const result = await this.orm.searchRead("property", [], []);
+        console.log(result);
+        this.state.records = result;
+    };
 }
 
 registry.category("actions").add("app_one.action_list_view", ListViewAction);
